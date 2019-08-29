4ebb65eaa3f126e962efeec3fda9dd171440122b
Christa <cmlhc99@gmail.com>
2019-08-30 11:40:28 +1200

    WithMenu

---
+++ New Unity Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine. SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    //public static SceneLoader instance = null;
+
+    private void Awake()
+    {/*
+        DontDestroyOnLoad(gameObject);
+
+        if(instance == null)
+        {
+            instance = this;
+
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void LoadBreakoutGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
